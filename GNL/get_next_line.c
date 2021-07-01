430e766ca709d3fc22a650678d6a4cb917f0b07e
Elouann BARGUILLET <ebarguil@e3r10p12.42.fr>
2021-07-01 10:10:50 +0200

    GNL recursive added

---
+++ GNL/get_next_line.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebarguil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/30 14:36:19 by ebarguil          #+#    #+#             */
+/*   Updated: 2021/06/30 15:31:08 by ebarguil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "get_next_line.h"
+
+int	gnl_rec(char **line, int index, int fd)
+{
+	char	buf;
+	int	x;
+
+	x = read(fd, &buf, 1);
+	if (x == 1 && buf != '\n')
+	{
+		x = gnl_rec(line, index + 1, fd);
+		if (x != -1)
+			(*line)[index] = buf;
+	}
+	else if (x != -1)
+	{
+		*line = (char *)malloc(sizeof(char) * (index + 1));
+		if (*line != NULL)
+			(*line)[index] = '\0';
+		else
+			x = -1;
+	}
+	return (x);
+}
+
+int	get_next_line(char **line)
+{
+	return (gnl_rec(line, 0, 0));
+}
---
+++ GNL/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebarguil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/30 14:39:22 by ebarguil          #+#    #+#             */
+/*   Updated: 2021/06/30 14:39:37 by ebarguil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+int	get_next_line(char **line);
+
+#endif
